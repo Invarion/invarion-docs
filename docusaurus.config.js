af7bb5d57157dfb50e84994d69f96186cf9777c8
Eduard Lepner <eduard@invarion.com>
2022-11-29 11:48:21 +0100

    remove sample blogs, set proper links and names in the config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,8 +16,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'invarion', // Usually your GitHub org/user name.
+  projectName: 'invarion-docs', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -65,7 +65,7 @@ const config = {
             label: 'Docs',
           },
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/invarion/invarion-docs',
             label: 'GitHub',
             position: 'right',
           },
