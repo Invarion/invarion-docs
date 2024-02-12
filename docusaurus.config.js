a9fca6c7957c65bc3a4996c812b180ee5a31659d
Lukasz <lukasz@invarion.com>
2024-02-12 12:00:38 +0000

    ignore broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,46 +1,46 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-import { themes as prismThemes } from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Invarion doc',
-  tagline: 'Dinosaurs are meh',
-  url: 'https://invarion.github.io/',
-  baseUrl: '/invarion-docs/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'throw',
-  favicon: 'img/cropped-favicon-32x32.png',
+  title: "Invarion doc",
+  tagline: "Dinosaurs are meh",
+  url: "https://invarion.github.io/",
+  baseUrl: "/invarion-docs/",
+  onBrokenLinks: "ignore",
+  onBrokenMarkdownLinks: "ignore",
+  favicon: "img/cropped-favicon-32x32.png",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'invarion', // Usually your GitHub org/user name.
-  projectName: 'invarion-docs', // Usually your repo name.
+  organizationName: "invarion", // Usually your GitHub org/user name.
+  projectName: "invarion-docs", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
           //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          routeBasePath: '/',
+          routeBasePath: "/",
         },
         blog: false,
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -49,63 +49,69 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      metadata: [{ name: 'keywords', content: 'rapid online, rapid plan cloud, rapidplan, rapid path online, documentation, what\'s new' }],
+      metadata: [
+        {
+          name: "keywords",
+          content:
+            "rapid online, rapid plan cloud, rapidplan, rapid path online, documentation, what's new",
+        },
+      ],
       navbar: {
         // title: 'Invarion Docs',
         logo: {
-          alt: 'Invarion logo',
-          src: 'img/logo.svg',
-          srcDark: 'img/logo-dark.svg'
+          alt: "Invarion logo",
+          src: "img/logo.svg",
+          srcDark: "img/logo-dark.svg",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Docs',
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Docs",
           },
           {
-            href: 'https://github.com/invarion/invarion-docs',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/invarion/invarion-docs",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'light',
+        style: "light",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Intro',
-                to: '/intro',
+                label: "Intro",
+                to: "/intro",
               },
             ],
           },
           {
-            title: 'Other resources',
+            title: "Other resources",
             items: [
               {
-                label: 'Discover our products',
-                href: 'https://invarion.com/',
+                label: "Discover our products",
+                href: "https://invarion.com/",
               },
               {
-                label: 'Contact us',
-                href: 'https://invarion.com/contact/',
+                label: "Contact us",
+                href: "https://invarion.com/contact/",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Blog',
-                href: 'https://invarion.com/blog/',
+                label: "Blog",
+                href: "https://invarion.com/blog/",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                label: "GitHub",
+                href: "https://github.com/facebook/docusaurus",
               },
             ],
           },
